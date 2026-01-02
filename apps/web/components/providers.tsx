99fa94eeab15d60eee9617b8339e9d7ebefddf95
Abdulahad <sparksuccessdayly@gmail.com>
2026-01-02 06:47:27 +0500

    Setup Clerk

--- apps/web/components/providers.tsx
+++ apps/web/components/providers.tsx
@@ -1,14 +1,19 @@
 "use client"
 
 import * as React from "react"
-import { ConvexProvider, ConvexReactClient } from "convex/react"
+import { ConvexReactClient } from "convex/react"
+import { ClerkProvider } from "@clerk/nextjs"
+import { ConvexProviderWithClerk } from 'convex/react-clerk'
+import { useAuth } from '@clerk/nextjs'
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ConvexProvider client={convex}>
+    <ClerkProvider>
+    <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
       {children}
-    </ConvexProvider>
+    </ConvexProviderWithClerk>
+    </ClerkProvider>
   )
 }
