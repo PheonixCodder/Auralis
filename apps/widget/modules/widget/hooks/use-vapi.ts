7da121be97e225f99f3911e69bffb98c0f9ba73e
Abdulahad <sparksuccessdayly@gmail.com>
2026-01-02 19:50:47 +0500

    Voice Assistant

---
+++ apps/widget/modules/widget/hooks/use-vapi.ts
@@ -0,0 +1,80 @@
+/* eslint-disable turbo/no-undeclared-env-vars */
+import Vapi from "@vapi-ai/web";
+import { useEffect, useState } from "react";
+
+interface TranscriptMessage {
+  role: "user" | "assistant";
+  text: string;
+}
+
+export const useVapi = () => {
+  const [vapi, setVapi] = useState<Vapi | null>(null);
+  const [isConnected, setIsConnected] = useState(false);
+  const [isConnecting, setIsConnecting] = useState(false);
+  const [isSpeaking, setIsSpeaking] = useState(false);
+  const [transcript, setTranscript] = useState<TranscriptMessage[]>([]);
+
+  useEffect(() => {
+    const vapiInstance = new Vapi(process.env.VAPI_PUBLIC_API_KEY!);
+    setVapi(vapiInstance);
+
+    vapiInstance.on("call-start", () => {
+      setIsConnected(true);
+      setIsConnecting(false);
+      setTranscript([]);
+    });
+
+    vapiInstance.on("call-end", () => {
+      setIsConnected(false);
+      setIsConnecting(false);
+      setIsSpeaking(false);
+    });
+
+    vapiInstance.on("speech-start", () => {
+      setIsSpeaking(true);
+    });
+
+    vapiInstance.on("speech-end", () => {
+      setIsSpeaking(false);
+    });
+
+    vapiInstance.on("error", (error) => {
+      console.log(error);
+      setIsConnecting(false);
+    });
+
+    vapiInstance.on("message", (message) => {
+      if (message.type === "transcript" && message.transcriptType === "final") {
+        setTranscript((prev) => [
+          ...prev,
+          {
+            role: message.role === "user" ? "user" : "assistant",
+            text: message.transcript,
+          },
+        ]);
+      }
+    });
+    return () => {
+      vapiInstance.stop();
+    };
+  }, []);
+  const startCall = () => {
+    setIsConnecting(true);
+    if (vapi) {
+      vapi.start("2a0bb009-6356-4bca-a65c-d49757088669");
+    }
+  };
+  const endCall = () => {
+    if (vapi) {
+      vapi.stop();
+    }
+  };
+  return {
+    isSpeaking,
+    isConnecting,
+    isConnected,
+    transcript,
+    startCall,
+    endCall,
+  };
+};
