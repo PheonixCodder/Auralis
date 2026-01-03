ab8f8a9afafd800dcd7c3a262a9c0bd8c17e7562
Abdulahad <sparksuccessdayly@gmail.com>
2026-01-03 12:03:20 +0500

    Create Widget Session

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -2,6 +2,7 @@
 
 import { WidgetFooter } from "../components/widget-footer";
 import { WidgetHeader } from "../components/widget-header";
+import { WidgetAuthScreen } from "../screens/widget-auth-screen";
 
 interface Props {
     organizationId: string
@@ -9,15 +10,8 @@ interface Props {
 
 export const WidgetView = ({ organizationId }: Props) => {
     return (
-        <main className="flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted font-semibold">
-            <WidgetHeader>
-                <div className="flex flex-col justify-between gap-y-2 px-2 py-6">
-                    <p className="text-3xl">Hi there!</p>
-                    <p className="text-lg">How can we help you today?</p>
-                </div>
-            </WidgetHeader>
-            <div className="flex flex-1"></div>
-            <WidgetFooter />
+        <main className="min-h-screen min-w-screen flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted font-semibold">
+            <WidgetAuthScreen />
         </main>
     )
 }
--- packages/backend/convex/_generated/api.d.ts
+++ packages/backend/convex/_generated/api.d.ts
@@ -8,13 +8,17 @@
  * @module
  */
 
+import type * as public_contactSessions from "../public/contactSessions.js";
+
 import type {
   ApiFromModules,
   FilterApi,
   FunctionReference,
 } from "convex/server";
 
-declare const fullApi: ApiFromModules<{}>;
+declare const fullApi: ApiFromModules<{
+  "public/contactSessions": typeof public_contactSessions;
+}>;
 
 /**
  * A utility for referencing Convex functions in your app's public API.
