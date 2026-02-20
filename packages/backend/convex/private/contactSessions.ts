499d7c27cd848d596b5e603a958cf25441d4f103
Ubaidullah Ismail <abdulahadismail.dev@gmail.com>
2026-02-20 01:40:50 -0800

    Setup Contact Panel

---
+++ packages/backend/convex/private/contactSessions.ts
@@ -0,0 +1,39 @@
+import { ConvexError, v } from "convex/values";
+import { query } from "../_generated/server";
+
+export const getOneByConversationId = query({
+  args: {
+    conversationId: v.id("conversations"),
+  },
+  handler: async (ctx, { conversationId }) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (identity === null) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Identity not found",
+      });
+    }
+    const orgId = identity.orgId as string;
+    if (!orgId) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Organization not found",
+      });
+    }
+    const conversation = await ctx.db.get(conversationId);
+    if (!conversation) {
+      throw new ConvexError({
+        code: "NOT_FOUND",
+        message: "Conversation not found",
+      });
+    }
+    if (conversation.organizationId !== orgId) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Invalid Organization ID",
+      });
+    }
+    const contactSession = await ctx.db.get(conversation.contactSessionId);
+    return contactSession;
+  },
+});
